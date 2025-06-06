9215d958ff77d5afb8ad953a75c31aa26f079ac1
RayhanFaizAdzikra <rayhankng266@gmail.com>
2025-06-06 21:33:29 +0700

    progres4

--- adminpanel/login.php
+++ adminpanel/login.php
@@ -8,8 +8,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href=../bootstrap/css/boostrap.min.css>
 </head>
+
+<style>
+    .main{
+        height: 100vh;
+    }
+
+    .login-box{
+        width: 500px;
+        height: 300px;
+        border: solid 1px;
+    }
+</style>
+
 <body>
-    
+   <div class="main d-flex justify-content-center align-items-center">
+    <div class="login-box"></div>
+   </div>
 </body>
 </html>

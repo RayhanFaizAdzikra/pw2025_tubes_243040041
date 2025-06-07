98cb40ae48d4650d50471d86054d2c376b139c34
RayhanFaizAdzikra <rayhankng266@gmail.com>
2025-06-07 23:47:10 +0700

    progres8

--- adminpanel/index.php
+++ adminpanel/index.php
@@ -8,8 +8,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../fontawesome/css/fontawesome.min.css">
 </head>
 <body>
-    <h2>Halo <?php echo $_SESSION['username']?></h2>
+    <?php require "navbar.php"; ?>
+    <div class="container mt-5">
+        <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+    <li class="breadcrumb-item active" aria-current="page">
+       <i class="fas fa-home"></i> Home
+    </li>
+    </ol>
+        </nav>
+        <h2>Halo <?php echo $_SESSION['username']?></h2>
+    </div>
+
+    <script src="">../bootstrap/js/bootstrap.bundle.min.js</script>
+    <script src="../fontawesome/js/all.min.js"></script>
 </body>
 </html>

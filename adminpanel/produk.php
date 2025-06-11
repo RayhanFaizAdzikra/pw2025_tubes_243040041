2e219eb91cd3c111fc5c44dd43e8c16314305e47
RayhanFaizAdzikra <rayhankng266@gmail.com>
2025-06-11 21:11:59 +0700

    progres13

--- adminpanel/produk.php
+++ adminpanel/produk.php
@@ -1,6 +1,9 @@
 <?php
     require "session.php";
     require "../koneksi.php";
+
+    $query = mysqli_query($conn, "SELECT * FROM dokter");
+    $jumlahDokter = mysqli_num_rows($query);
 ?>
 
 <!DOCTYPE html>
@@ -35,10 +38,20 @@
         </nav>
 
         <!--tambah doctor-->
-     <div class="my-5 col-12 col-md-6"></div>
+     <div class="my-5 col-12 col-md-6">
+        <h3>Tambah Doctor</h3>
+
+        <form action="" method="post" enctype="multipart/form-data">
+            <div>
+                <label for="nama">Nama</label>
+                <input type="text" id="nama" name="nama" class="form-control" autocomplete="off">
+            </div>
+        </form>
+     </div>
+     
 
      <div class="mt-3">
-         <h2>List Docter</h2>
+         <h2>List Doctor</h2>
 
          <div class="table-responsive mt-4">
             <table class="table">
@@ -52,7 +65,30 @@
                     </tr>
                 </thead>
                 <tbody>
-
+                    <?php
+                     if($jumlahDokter==0){
+                    ?>
+                         <tr>
+                            <td colspan=5 class="text-center">Data Doctor Tidak Tersedia</td>
+                        </tr>
+                    <?php
+                     }
+                     else {
+                        $jumlah = 1;
+                        while($data=mysqli_fetch_array($query)){
+                    ?>
+                        <tr>
+                            <td><?php echo $jumlah; ?></td>
+                            <td><?php echo $data['nama']; ?></td>
+                            <td><?php echo $data['kategori_id']; ?></td>
+                            <td><?php echo $data['jadwal_praktek']; ?></td>
+                            <td><?php echo $data['ketersediaan_antrian']; ?></td>
+                        </tr>
+                    <?php
+                        $jumlah++;
+                        }
+                     }
+                    ?>
                 </tbody>
             </table>
          </div>

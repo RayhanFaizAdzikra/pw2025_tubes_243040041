6d13f22c7e5e2c516123cb930226cb2c919af6c4
RayhanFaizAdzikra <rayhankng266@gmail.com>
2025-06-12 16:09:39 +0700

    progres14

--- adminpanel/produk.php
+++ adminpanel/produk.php
@@ -4,6 +4,18 @@
 
     $query = mysqli_query($conn, "SELECT * FROM dokter");
     $jumlahDokter = mysqli_num_rows($query);
+
+    $queryKategori = mysqli_query($conn, "SELECT * FROM kategori");
+
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+    return $randomString;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -19,6 +31,10 @@
 <style>
      .no-decoration {
         text-decoration: none;
+    }
+
+    form div{
+        margin-bottom: 10px;
     }
 </style>
 
@@ -44,11 +60,97 @@
         <form action="" method="post" enctype="multipart/form-data">
             <div>
                 <label for="nama">Nama</label>
-                <input type="text" id="nama" name="nama" class="form-control" autocomplete="off">
+                <input type="text" id="nama" name="nama" class="form-control" autocomplete="off" required>
             </div>
+            <div>
+                <label for="kategori">Spesialis Doctor</label>
+            <select name="kategori" id="kategori" class="form-control" required>
+                <option value="">Pilih Spesialis Doctor</option>
+                <?php
+                    while($data=mysqli_fetch_array($queryKategori)){
+                    ?>
+                        <option value="<?php echo $data['id']?>"><?php echo $data['nama']?></option>
+                    <?php
+                    }
+                ?>
+            </select>
+    </div>
+    <div>
+        <label for="jadwal_praktek">Jadwal Praktek</label>
+        <input type="text" class="form-control" name="jadwal_praktek" required>
+    </div>
+
+    <div>
+        <label for="foto">Foto</label>
+        <input type="file" name="foto" id="foto" class="form-control">
+    </div>
+
+    <div>
+        <label for="detail">Detail</label>
+        <textarea name="detail" id="detail" class="form-control"></textarea>
+    </div>
+
+    <div>
+        <label for="ketersediaan_antrian">Ketersediaan Antrian</label>
+        <select name="ketersediaan_antrian" id="ketersediaan_antrian" class="form-control">
+            <option value="">Pilih Ketersediaan Antrian</option>
+            <option value="tersedia">Tersedia</option>
+            <option value="habis">Tidak Tersedia</option>
+        </select>
+    </div>
+    <div>
+        <button type="submit" class="btn btn-primary mt-3" name="simpan">Simpan</button>
+    </div>
         </form>
-     </div>
-     
+
+        <?php
+            if(isset($_POST['simpan'])){
+                $nama = htmlspecialchars($_POST['nama']);
+                $kategori = htmlspecialchars($_POST['kategori']);
+                $jadwal_praktek = htmlspecialchars($_POST['jadwal_praktek']);
+                $detail = htmlspecialchars($_POST['detail']);
+                $ketersediaanantrian = htmlspecialchars($_POST['ketersediaan_antrian']);
+
+                $target_dir = "../image/";
+                $nama_file = basename($_FILES['foto']['name']);
+                $target_file = $target_dir . $nama_file;
+                $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                $image_size = $_FILES['foto']['size'];
+                $random_name = generateRandomString(20);
+
+                if($nama=='' || $kategori=='' || $jadwal_praktek==''){
+                    ?>
+                    <div class="alert alert-warning mt-3" role="alert">
+                        Nama, Spesialis Doctor, dan Jadwal Praktek harus diisi!!!
+                    </div>
+                    <?php
+                }
+                else {
+                    if($nama_file != ''){
+                        if($image_size > 5000000){
+                            ?>
+                            <div class="alert alert-warning mt-3" role="alert">
+                                Ukuran Foto Terlalu Besar, Maksimal 5MB!!!
+                             </div>
+                            <?php
+                        }
+                        else {
+                            if($imageFileType != 'jpg' && $imageFileType != 'png' && $imageFileType != 'jpeg'){
+                            ?>
+                             <div class="alert alert-warning mt-3" role="alert">
+                                File Harus Bertipe JPG, PNG, atau JPEG!!!
+                             </div>
+                            <?php
+                        }
+                        else{
+                                move_uploaded_file($_FILES['foto']['tmp_name'], $target_dir . $random_name . "." . $imageFileType);
+                            }
+                        }
+                    }
+                }
+            }
+        ?>
+     </div> 
 
      <div class="mt-3">
          <h2>List Doctor</h2>
@@ -59,7 +161,7 @@
                     <tr>
                         <th>No</th>
                         <th>Nama</th>
-                        <th>Kategori</th>
+                        <th>Spesialis Doctor</th>
                         <th>Jadwal Praktek</th>
                         <th>Ketersediaan Antrian</th>
                     </tr>

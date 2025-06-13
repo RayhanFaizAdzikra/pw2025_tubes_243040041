f6c0f753de27a6618f49d22557d425cf173647e6
RayhanFaizAdzikra <rayhankng266@gmail.com>
2025-06-14 06:08:20 +0700

    progres25

--- produk-detail.php
+++ produk-detail.php
@@ -40,6 +40,8 @@
         </div>
     </div>
 
+    
+
     <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="../fontawesome/js/all.min.js"></script>
 </body>
---
+++ tentang-kami.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RS Modern | Tentang Kami</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php require "navbar.php"?>
+
+    <div class="container-fluid background-produk d-flex align-items-center">
+        <div class="container">
+            <h1 class="text-white text-center">Tentang Kami</h1>
+        </div>
+    </div>
+
+    <div class="container-fluid py-5">
+        <div class="container fs-5 text-center">
+            <p>Kami adalah layanan kesehatan yang berkomitmen memberikan pelayanan terbaik, profesional, dan ramah bagi seluruh pasien. Kesehatan Anda adalah prioritas kami.RS Modern hadir sebagai pusat pelayanan kesehatan yang mengutamakan kualitas, keamanan, dan kenyamanan pasien. Dengan dukungan tenaga medis profesional dan fasilitas modern, dengan visi menjadi pusat layanan kesehatan unggulan di wilayah Kuningan dan sekitarnya, kami terus berinovasi dalam pelayanan, menjaga kepercayaan pasien, dan membangun sistem kesehatan yang profesional dan berkelanjutan. Kami siap melayani masyarakat dengan sepenuh hati.</p>
+            <p>RS Modern berdiri dengan semangat untuk menjadi mitra kesehatan yang dapat diandalkan oleh masyarakat. Kami memahami bahwa pelayanan kesehatan bukan hanya soal tindakan medis, tetapi juga soal kepercayaan, kepedulian, dan kenyamanan. Oleh karena itu, setiap langkah kami selalu berpusat pada kebutuhan pasien, mulai dari pemeriksaan awal, diagnosis, hingga perawatan lanjutan.</p>
+            <p>Didukung oleh tenaga medis profesional, perawat yang terlatih, serta fasilitas yang terus diperbarui, kami berkomitmen memberikan pelayanan yang cepat, tepat, dan humanis. RS Modern melayani berbagai kebutuhan kesehatan, mulai dari rawat jalan, rawat inap, layanan gawat darurat 24 jam, laboratorium, farmasi, hingga spesialisasi medis lainnya.</p>
+        </div>
+    </div>
+
+    <?php require "footer.php"?>
+
+    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../fontawesome/js/all.min.js"></script>
+</body>
+</html>
